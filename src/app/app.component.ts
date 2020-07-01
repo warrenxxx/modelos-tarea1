eb0eab08bf8b0e75bb2f6a6774b299b1d0c44710
Your Name <you@example.com>
2020-07-01 17:55:25 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var Raphael :any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  value="";
   title = 'modelos1';
+
+
+
+  ngOnInit(){
+
+          // Creates canvas 320 × 200 at 10, 50
+      var paper = Raphael(10, 50, 320, 200);
+
+
+      var circle = paper.circle(50, 40, 10);
+
+
+      circle.attr("fill", "#f00");
+
+      circle.attr("stroke", "#fff");
+      circle.animate({cy:150},2000);
+
+  }
+  getNumbers():number[]{
+    return this.value.split("-").map(e=>parseInt(e))    ;
+  }
+  change(){
+    console.log(this.getNumbers());
+  }
+  reloadCanvas(){
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatInputModule} from '@angular/material/input';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
